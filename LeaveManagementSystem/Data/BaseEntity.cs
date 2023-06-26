5bde82c39be36052f776a8f807ac78e0558845f9
L@b1yB <L@b1yB@DESKTOP-UL611K8>
2023-06-26 11:53:46 +0300

    Added Database Model

---
+++ LeaveManagementSystem/Data/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagementSystem.Data
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
